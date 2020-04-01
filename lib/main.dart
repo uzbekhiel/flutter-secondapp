02acb49407d7a18d1f5f8bcc1d95cd97a2d8152e
Saulo dos Santos Firmino <uzbekhiel@gmail.com>
2020-04-01 00:51:08 -0300

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:alcoolgas/widgets/input.widget.dart';
+import 'package:alcoolgas/widgets/logo.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alcool ou Gasolina',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcoolCtrl = new MoneyMaskedTextController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: [
+          Logo(),
+          Input(
+            label: "Gasolina",
+            ctrl: _gasCtrl,
+          ),
+          Input(
+            label: "Álcool",
+            ctrl: _alcoolCtrl,
+          ),
+          Container(
+            margin: EdgeInsets.all(30),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(60),
+            ),
+            child: FlatButton(
+              onPressed: () {},
+              child: Text(
+                "CALCULAR",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 25,
+                  fontFamily: "Big Shoulders Display",
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
